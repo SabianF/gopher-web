8c72eb34b87d8d18953cf7071ba62118c23f0040
SabianF <58588133+SabianF@users.noreply.github.com>
2026-03-22 13:39:20 -0700

    feat: basic webserver

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Received request for " + r.URL.Path)
+	w.WriteHeader(200)
+}
 
 func main() {
-	fmt.Println("Hello, there!")
+	http.HandleFunc("/", rootHandler)
+
+	PORT := 3000
+	fmt.Printf("Running server on port %d\n", PORT)
+	log.Fatal(
+		http.ListenAndServe(":" + strconv.Itoa(PORT), nil),
+	)
 }
