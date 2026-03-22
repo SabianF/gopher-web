0a15dd35d351d6437ec92461f470008d98a6d0e8
SabianF <58588133+SabianF@users.noreply.github.com>
2026-03-22 14:20:00 -0700

    feat: serve root page placeholder

--- main.go
+++ main.go
@@ -4,12 +4,17 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 )
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Received request for " + r.URL.Path)
-	w.WriteHeader(200)
+	root_page, err := os.ReadFile("features/core/presentation/templates/layout.html")
+	if (err != nil) {
+		panic(err)
+	}
+	w.Write(root_page)
 }
 
 func main() {
