cc4e3231698239cbb986a288c53a48ba1c6c8916
SabianF <58588133+SabianF@users.noreply.github.com>
2026-04-04 00:30:52 -0700

    feat: nesting templates

--- main.go
+++ main.go
@@ -2,23 +2,30 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"strconv"
 
-	"github.com/SabianF/gopher-web/features/core/presentation/templates"
+	data_repos "github.com/SabianF/gopher-web/features/core/data/repos"
 )
 
+var pages map[string]*template.Template
+
+func init() {
+	pages = make(map[string]*template.Template)
+
+	pages["home"] = template.Must(template.ParseFiles(
+		data_repos.GetRootPath() + "/features/core/presentation/templates/pages/home.html",
+		data_repos.GetRootPath() + "/features/core/presentation/templates/components/layout.html",
+	))
+}
+
 func rootHandler(w http.ResponseWriter, r *http.Request) {
-	layout_data := templates.LayoutData{
-		Title: "The Title",
-		MetaDescription: "This is the meta description.",
-		PageBody: "<p>Page body.</p>",
+	err := pages["home"].ExecuteTemplate(w, "layout", nil)
+	if (err != nil) {
+		fmt.Printf("%v", err)
 	}
-
-	root_page := templates.Layout(layout_data)
-
-	w.Write([]byte(root_page))
 }
 
 func main() {
